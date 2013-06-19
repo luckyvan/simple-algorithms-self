ce7ace5a12bf67df2ae1517feb05a8ebdc56fc65
luckyvan <luckyvan@luckyvan-VirtualBox.(none)>
2013-06-19 21:05:04 +0800

    add DList_iterator support

--- src/DList.h
+++ src/DList.h
@@ -1,46 +1,126 @@
 #ifndef MY_DLIST
 #define MY_DLIST
 #include "DLink.h"
+#include <cstddef>
+#include <iterator>
+
+using namespace std;
+
 namespace MyList{
-	template <class Elem>
-		class DList{
-			typedef DLink<Elem>* link_type;
-			protected:
-			link_type get_node(){
-				return new DLink<Elem>();	
+		struct _DList_iterator_base{
+			_DLink_base* _M_link;
+
+			_DList_iterator_base(){}
+			_DList_iterator_base(_DLink_base*  __x):
+				_M_link(__x){}
+
+			void _M_incr() { _M_link = _M_link->next; }
+			void _M_decr() { _M_link = _M_link->prev; }
+
+			bool operator==(const _DList_iterator_base& __x) const {
+				return _M_link == __x._M_link;
 			}
 
-			void put_node(link_type n){
+			bool operator!=(const _DList_iterator_base& __x) const {
+				return _M_link != __x._M_link;
+			}
+		};
+
+	template <class _Elem, class _Ref, class _Ptr>
+		struct _DList_iterator: public _DList_iterator_base{
+			//associated types
+		    typedef _Elem value_type;
+			typedef _Ptr  pointer;
+			typedef _Ref  reference;
+			typedef size_t	size_type;
+			typedef	ptrdiff_t difference_type;
+			typedef bidirectional_iterator_tag iterator_category;
+
+			//inner used
+			typedef _DList_iterator<_Elem, _Elem&, _Elem*> iterator;	
+			typedef _DList_iterator<_Elem, const _Elem&, const _Elem*> const_iterator;	
+			typedef _DList_iterator<_Elem, _Ref, _Ptr> _Self;
+			typedef _DLink<_Elem> _Link;
+
+			_DList_iterator(_DLink<_Elem>* __x):_DList_iterator_base(__x){}
+			_DList_iterator(){}
+			_DList_iterator(const iterator& __x): _DList_iterator_base(__x._M_link){}
+
+			reference operator*() const { return ((_Link*)_M_link)->element;}
+
+			_Self& operator++(){
+				this->_M_incr();
+				return *this;
+			}
+
+			_Self operator++(int){
+				_Self tmp = *this;
+				this->_M_incr();
+				return tmp;
+			}
+		    	
+
+			_Self& operator--(){
+				this->_M_decr();
+				return *this;
+			}
+
+			_Self operator--(int){
+				_Self tmp = *this;
+				this->_M_decr();
+				return tmp;
+			}
+		};
+
+	template <class Elem>
+		class DList{
+			typedef Elem value_type;
+			typedef Elem* pointer;
+			typedef Elem& reference;
+			typedef size_t size_type;
+			typedef ptrdiff_t difference_type;
+			typedef _DList_iterator<Elem, Elem&, Elem*> iterator;
+
+			typedef _DLink<Elem>* _Link;
+			protected:
+			_Link get_node(){
+				return new _DLink<Elem>();	
+			}
+
+			void put_node(_Link n){
 				delete n;
 			}
 
-			link_type create_node(const Elem& elem){
-				link_type n = get_node();
+			_Link create_node(const Elem& elem){
+				_Link n = get_node();
 				n->element = elem;
 			}
 
-			void destroy_node(link_type n){
+			void destroy_noda(_Link n){
 				put_node(n);
 			}
 
 			void empty_initialize(){
-				node = new DLink<Elem>(); //sentinel
+				node = new _DLink<Elem>(); //sentinel
 				node->next = node;
 			   	node->prev = node;	
 			}
 
-			void transfer(link_type pos, link_type first, link_type last){
-				last->prev->next = pos;
-				first->prev->next = last;
-				pos->prev->next = first;
-				link_type tmp = pos->prev;
-				pos->prev = last->prev;
-				last->prev = first->prev;
-				first->prev = tmp;
+			void transfer(iterator pos, iterator first, iterator last){
+				if(pos != last){
+					(last._M_link)->prev->next = pos._M_link;
+					(first._M_link)->prev->next = last._M_link;
+					pos._M_link->prev->next = first._M_link;
+
+					_DLink_base* tmp = pos._M_link->prev;
+					pos._M_link->prev = last._M_link->prev;
+					last._M_link->prev = first._M_link->prev;
+					first._M_link->prev = tmp;
+				}
 			}
 
 			private:
-			DLink<Elem> *node; // node ptr to the end sentinel, and the whole list forms a circle
+			_DLink<Elem> *node; // node ptr to the end sentinel, and the whole list forms a circle
 			public:
 			DList(){
 				empty_initialize();
@@ -59,26 +139,26 @@ namespace MyList{
 				clear();
 			}
 			
-			DLink<Elem>* begin(){
-				return node->next;
+			iterator begin(){
+				return (_Link)node->next;
 			}
 
-			DLink<Elem>* end(){
-				return node;
+			iterator end(){
+				return (_Link)node;
 			}
 
-			Elem front(){
-				return begin()->element;
+			reference front(){
+				return *begin();
 			}
 
-			Elem back(){
-				return end()->prev->element;
+			reference back(){
+				return *(--end());
 			}
 
 			size_t size(){
-				size_t s = 0;
-				for(DLink<Elem>* cur = begin(); cur != end(); 
-						s++, cur=cur->next);
+				size_type s = 0;
+				for(iterator cur = begin(); cur != end(); 
+						s++, cur++);
 				return s;
 			}
 
@@ -86,22 +166,22 @@ namespace MyList{
 				return begin() == end();
 			}
 
-			void insert(DLink<Elem>* pos, const Elem& elem){
-				link_type tmp = create_node(elem);
+			void insert(iterator pos, const Elem& elem){
+				_Link tmp = create_node(elem);
 
-				tmp->next = pos;
-				tmp->prev = pos->prev;
-				pos->prev->next = tmp;
+				tmp->next = pos._M_link;
+				tmp->prev = pos._M_link->prev;
+				pos._M_link->prev->next = tmp;
 
-				pos->prev= tmp;
+				pos._M_link->prev= tmp;
 			}
 
-			void erase(link_type pos){
-				link_type next = pos->next;
-				link_type prev = pos->prev;
+			void erase(iterator pos){
+				_Link next = _Link(pos._M_link->next);
+				_Link prev = _Link(pos._M_link->prev);
 				prev->next = next;
 				next->prev = prev;
-				destroy_node(pos);
+				destroy_node(_Link(pos._M_link));
 				return next;
 			}
 
@@ -114,19 +194,17 @@ namespace MyList{
 			}
 
 			void pop_front(){
-				DLink<Elem>* node = begin();
-				node->next = node->next;
-				delete node;
+				erase(begin());
 			}
 
 			void reverse(){
-				if(empty() || begin()->next == end()) return;
+				if(empty() || _Link(node->next)->next == node) return;
 
-				link_type first = begin();
-				first = first->next;
+				iterator first = begin();
+				++first; 
 				while(first != end()){
-					link_type old = first;
-					first = first->next;
+					iterator old = first;
+					++first;
 					transfer(begin(), old, first);
 				}
 			}
